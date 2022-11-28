830eec2de0ac5ac4a7aaebb017743220b826f660
jh1359753 <jh1359753@gmail.com>
2022-11-28 17:57:04 +0900

    feature-bok: Booking 작업 파일 생성 및 작업
    기존 MeetingRoom 제거 및 Booking 파일 생성 및 작업(insert)

--- src/main/java/org/shiftworksboot/entity/Booking.java
+++ src/main/java/org/shiftworksboot/entity/Booking.java
@@ -3,6 +3,7 @@ package org.shiftworksboot.entity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.shiftworksboot.dto.BookingDto;
 
 import javax.persistence.*;
 
@@ -13,18 +14,26 @@ import javax.persistence.*;
 public class Booking {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     @Column(name = "book_id")
     private int book_id;
 
-    private String book_date;
-    private String book_begin;
-    private int book_end;
     private String book_title;
     private String book_content;
+    private String book_date;
+    private String book_begin;
+    private String meetingRoom;
 
-    @ManyToOne
-    @JoinColumn(name = "rsc_id")
-    private MeetingRoom meetingRoom;
+    public static Booking createBooking(BookingDto bookingDto){
+        Booking booking = new Booking();
+        booking.setBook_title(bookingDto.getBook_title());
+        booking.setBook_content(bookingDto.getBook_content());
+        booking.setBook_date(bookingDto.getBook_date());
+        booking.setBook_begin(bookingDto.getBook_begin());
+        booking.setMeetingRoom(bookingDto.getMeetingRoom());
+
+        return booking;
+    }
+
 
 }
